ae70347f317a974a7149af22648b42d32f7c3123
SinTiger <qvadim@gmail.com>
2015-07-04 14:01:49 +0300

    build 06

--- Survival_on_island/Island.Designer.cs
+++ Survival_on_island/Island.Designer.cs
@@ -30,9 +30,9 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Island));
             this.buttonContin = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.buttonVer1 = new System.Windows.Forms.Button();
+            this.buttonVer2 = new System.Windows.Forms.Button();
+            this.buttonVer3 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
@@ -55,45 +55,47 @@
             this.buttonContin.UseVisualStyleBackColor = false;
             this.buttonContin.Click += new System.EventHandler(this.buttonContin_Click);
             // 
-            // button1
+            // buttonVer1
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.ForeColor = System.Drawing.Color.Lime;
-            this.button1.Location = new System.Drawing.Point(615, 53);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(148, 34);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Равнина";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.buttonVer1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonVer1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonVer1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonVer1.ForeColor = System.Drawing.Color.Lime;
+            this.buttonVer1.Location = new System.Drawing.Point(615, 53);
+            this.buttonVer1.Name = "buttonVer1";
+            this.buttonVer1.Size = new System.Drawing.Size(148, 34);
+            this.buttonVer1.TabIndex = 6;
+            this.buttonVer1.Text = "Равнина";
+            this.buttonVer1.UseVisualStyleBackColor = false;
+            this.buttonVer1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // buttonVer2
             // 
-            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button2.ForeColor = System.Drawing.Color.Yellow;
-            this.button2.Location = new System.Drawing.Point(615, 93);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(148, 34);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "Лес";
-            this.button2.UseVisualStyleBackColor = false;
+            this.buttonVer2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonVer2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonVer2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonVer2.ForeColor = System.Drawing.Color.Yellow;
+            this.buttonVer2.Location = new System.Drawing.Point(615, 93);
+            this.buttonVer2.Name = "buttonVer2";
+            this.buttonVer2.Size = new System.Drawing.Size(148, 34);
+            this.buttonVer2.TabIndex = 7;
+            this.buttonVer2.Text = "Лес";
+            this.buttonVer2.UseVisualStyleBackColor = false;
+            this.buttonVer2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button3
+            // buttonVer3
             // 
-            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button3.ForeColor = System.Drawing.Color.Red;
-            this.button3.Location = new System.Drawing.Point(615, 133);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(148, 34);
-            this.button3.TabIndex = 8;
-            this.button3.Text = "Болото";
-            this.button3.UseVisualStyleBackColor = false;
+            this.buttonVer3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonVer3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonVer3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonVer3.ForeColor = System.Drawing.Color.Red;
+            this.buttonVer3.Location = new System.Drawing.Point(615, 133);
+            this.buttonVer3.Name = "buttonVer3";
+            this.buttonVer3.Size = new System.Drawing.Size(148, 34);
+            this.buttonVer3.TabIndex = 8;
+            this.buttonVer3.Text = "Болото";
+            this.buttonVer3.UseVisualStyleBackColor = false;
+            this.buttonVer3.Click += new System.EventHandler(this.buttonVer3_Click);
             // 
             // pictureBox1
             // 
@@ -151,9 +153,9 @@
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonVer3);
+            this.Controls.Add(this.buttonVer2);
+            this.Controls.Add(this.buttonVer1);
             this.Controls.Add(this.buttonContin);
             this.Name = "Island";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -169,9 +171,9 @@
         #endregion
 
         private System.Windows.Forms.Button buttonContin;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button buttonVer1;
+        private System.Windows.Forms.Button buttonVer2;
+        private System.Windows.Forms.Button buttonVer3;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox3;
--- Survival_on_island/Island.cs
+++ Survival_on_island/Island.cs
@@ -16,6 +16,20 @@ namespace Survival_on_island
             InitializeComponent();
         }
 
+        //++++++++++++++++++
+        //Переменные. ресурсы.
+        //++++++++++++++++++
+        int eat = 0;
+        int wood = 0;
+        int rock = 0;
+
+        int use1_var1_min = 1;
+        int use1_var1_max = 3;
+        int use1_var2_min = 3;
+        int use1_var2_max = 5;
+        int use1_var3_min = 5;
+        int use1_var3_max = 7;
+
         private void buttonContin_Click(object sender, EventArgs e)
         {
 
@@ -23,7 +37,23 @@ namespace Survival_on_island
 
         private void button1_Click(object sender, EventArgs e)
         {
-            label_eat.Text += "2";
+            Random rand = new Random();
+            eat += rand.Next(use1_var1_min, use1_var1_max+1);
+            label_eat.Text = Convert.ToString(eat);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            eat += rand.Next(use1_var2_min, use1_var2_max+1);
+            label_eat.Text = Convert.ToString(eat);
+        }
+
+        private void buttonVer3_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            eat += rand.Next(use1_var3_min, use1_var3_max+1);
+            label_eat.Text = Convert.ToString(eat);
         }
     }
 }
