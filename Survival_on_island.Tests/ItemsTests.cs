fcfc73d36b5295741057b36abbc2b0bc3a804dd0
Alexander Pervakov <frost.nzcr4@jagmort.com>
2015-07-24 04:50:32 +0300

    Add tests for Items class

---
+++ Survival_on_island.Tests/ItemsTests.cs
@@ -0,0 +1,43 @@
+﻿using Xunit;
+using Xunit.Extensions;
+
+namespace Survival_on_island.Tests
+{
+    public class ItemsTests
+    {
+        [Theory]
+        [InlineData("Каменный нож", "Самодельный каменный нож.\nУрон: +1", 0, 1, 1)]
+        public void Items(string name, string text, int value, int maxValue, int type)
+        {
+            Items item = new Items(name, text, value, maxValue, type);
+            Assert.Equal(name, item.name);
+            Assert.Equal(text, item.text);
+            Assert.Equal(value, item.value);
+            Assert.Equal(maxValue, item.maxValue);
+            Assert.Equal(type, item.type);
+            Assert.Equal(0, item.equip);
+        }
+
+        [Theory]
+        [InlineData("Каменный нож", "Самодельный каменный нож.\nУрон: +1", 0, 1, 1)]
+        public void ItemAdd(string name, string text, int value, int maxValue, int type)
+        {
+            Items item = new Items(name, text, value, maxValue, type);
+            item.ItemAdd();
+			Assert.Equal(value + 1, item.value);
+			item.ItemAdd();
+			Assert.Equal(value + 1, item.value);
+        }
+
+        [Theory]
+        [InlineData("Каменный нож", "Самодельный каменный нож.\nУрон: +1", 1, 1, 1)]
+        public void ItemMinus(string name, string text, int value, int maxValue, int type)
+        {
+            Items item = new Items(name, text, value, maxValue, type);
+            item.ItemMinus();
+			Assert.Equal(value - 1, item.value);
+			item.ItemMinus();
+			Assert.Equal(0, item.value);
+        }
+    }
+}
