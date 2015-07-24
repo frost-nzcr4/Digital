a93a8fc28cf50d31f32a00a31ba1eb2c0d2dff58
Alexander Pervakov <frost.nzcr4@jagmort.com>
2015-07-24 17:05:29 +0300

    [Pers] Test additional chars

--- Survival_on_island.Tests/PersTests/PersFileTests.cs
+++ Survival_on_island.Tests/PersTests/PersFileTests.cs
@@ -10,8 +10,9 @@ namespace Survival_on_island.Tests.PersTests
         //private string projectPath = Directory.GetParent(System.AppDomain.CurrentDomain.BaseDirectory).Parent.FullName;
 
         [Theory]
-        [InlineData("Герой", 1, 2, 3, 4, 5, 6)]
-        public void Load(string name, int NavSob, int NavHunt, int NavFish, int NavBuild, int NavNauka, int NavMed)
+        [InlineData("Герой", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)]
+        public void Load(string name, int NavSob, int NavHunt, int NavFish, int NavBuild, int NavNauka, int NavMed,
+            int hp, int def, int od, int damage)
         {
             string fixturePersIn = Path.Combine(projectPath, "PersTests", "fixtures", "Pers.json");
 
@@ -23,13 +24,18 @@ namespace Survival_on_island.Tests.PersTests
             Assert.Equal(NavBuild, pers.NavBuild);
             Assert.Equal(NavNauka, pers.NavNauka);
             Assert.Equal(NavMed, pers.NavMed);
+            Assert.Equal(hp, pers.hp);
+            Assert.Equal(def, pers.def);
+            Assert.Equal(od, pers.od);
+            Assert.Equal(damage, pers.damage);
         }
 
         [Theory]
-        [InlineData("Герой", 1, 2, 3, 4, 5, 6)]
-        public void Save(string name, int NavSob, int NavHunt, int NavFish, int NavBuild, int NavNauka, int NavMed)
+        [InlineData("Герой", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)]
+        public void Save(string name, int NavSob, int NavHunt, int NavFish, int NavBuild, int NavNauka, int NavMed,
+            int hp, int def, int od, int damage)
         {
-            Pers pers1 = new Pers(name, NavSob, NavHunt, NavFish, NavBuild, NavNauka, NavMed);
+            Pers pers1 = new Pers(name, NavSob, NavHunt, NavFish, NavBuild, NavNauka, NavMed, hp, def, od, damage);
             string fixturePersOut = Path.Combine(projectPath, "PersTests", "fixtures", pers1.EscapeName() + ".json");
 
             PersFile.Save(pers1, fixturePersOut);
@@ -42,6 +48,10 @@ namespace Survival_on_island.Tests.PersTests
             Assert.Equal(pers1.NavBuild, pers2.NavBuild);
             Assert.Equal(pers1.NavNauka, pers2.NavNauka);
             Assert.Equal(pers1.NavMed, pers2.NavMed);
+            Assert.Equal(pers1.hp, pers2.hp);
+            Assert.Equal(pers1.def, pers2.def);
+            Assert.Equal(pers1.od, pers2.od);
+            Assert.Equal(pers1.damage, pers2.damage);
         }
     }
 }
--- Survival_on_island.Tests/PersTests/PersTests.cs
+++ Survival_on_island.Tests/PersTests/PersTests.cs
@@ -10,10 +10,11 @@ namespace Survival_on_island.Tests.PersTests
         //private string projectPath = Directory.GetParent(System.AppDomain.CurrentDomain.BaseDirectory).Parent.FullName;
 
         [Theory]
-        [InlineData("Герой", 1, 2, 3, 4, 5, 6)]
-        public void Pers(string name, int NavSob, int NavHunt, int NavFish, int NavBuild, int NavNauka, int NavMed)
+        [InlineData("Герой", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)]
+        public void Pers(string name, int NavSob, int NavHunt, int NavFish, int NavBuild, int NavNauka, int NavMed,
+            int hp, int def, int od, int damage)
         {
-            Pers pers = new Pers(name, NavSob, NavHunt, NavFish, NavBuild, NavNauka, NavMed);
+            Pers pers = new Pers(name, NavSob, NavHunt, NavFish, NavBuild, NavNauka, NavMed, hp, def, od, damage);
             Assert.Equal(name, pers.name);
             Assert.Equal(NavSob, pers.NavSob);
             Assert.Equal(NavHunt, pers.NavHunt);
@@ -21,12 +22,16 @@ namespace Survival_on_island.Tests.PersTests
             Assert.Equal(NavBuild, pers.NavBuild);
             Assert.Equal(NavNauka, pers.NavNauka);
             Assert.Equal(NavMed, pers.NavMed);
+            Assert.Equal(hp, pers.hp);
+            Assert.Equal(def, pers.def);
+            Assert.Equal(od, pers.od);
+            Assert.Equal(damage, pers.damage);
         }
 
         [Fact]
         public void EscapeName()
         {
-            Pers pers = new Pers("Герой", 1, 2, 3, 4, 5, 6);
+            Pers pers = new Pers("Герой", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
             Assert.Equal("Герой", pers.EscapeName());
             pers.name = "\\//:?";
             Assert.Equal("\\//:?", pers.EscapeName());
@@ -35,7 +40,7 @@ namespace Survival_on_island.Tests.PersTests
         [Fact]
         public void SaveWithPredefinedName()
         {
-            Pers pers = new Pers("Герой", 1, 2, 3, 4, 5, 6);
+            Pers pers = new Pers("Герой", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
             string save_folder = Path.Combine(projectPath, "saves");
             string pers_saved = Path.Combine(projectPath, "saves", pers.EscapeName() + ".json");
 
@@ -49,7 +54,7 @@ namespace Survival_on_island.Tests.PersTests
         [Fact]
         public void SaveWithSpecifiedName()
         {
-            Pers pers = new Pers("Герой", 1, 2, 3, 4, 5, 6);
+            Pers pers = new Pers("Герой", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
             string save_folder = Path.Combine(projectPath, "saves");
             string pers_saved = Path.Combine(projectPath, "saves", pers.EscapeName() + "-specified-name.json");
 
