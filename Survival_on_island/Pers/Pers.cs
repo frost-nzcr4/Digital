d3bb18754285a5d9d10ccdb5d8894cb450c8d5a1
Alexander Pervakov <frost.nzcr4@jagmort.com>
2015-07-24 15:57:34 +0300

    Split Pers to Pers and PersFile

--- Survival_on_island/Pers/Pers.cs
+++ Survival_on_island/Pers/Pers.cs
@@ -1,6 +1,4 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.IO;
+﻿using System.IO;
 
 namespace Survival_on_island
 {
@@ -65,22 +63,6 @@ namespace Survival_on_island
         public void Save(string filename)
         {
             PersFile.Save(this, filename);
-        }
-    }
-
-    public static class PersFile
-    {
-        public static Pers Load(string filename)
-        {
-            var pers_file = File.ReadAllText(filename);
-            Pers pers = JsonConvert.DeserializeObject<Pers>(pers_file);
-            return pers;
-        }
-
-        public static void Save(Pers pers, string filename)
-        {
-            string json = JsonConvert.SerializeObject(pers, Formatting.Indented);
-            File.WriteAllText(filename, json);
         }
     }
 }
---
+++ Survival_on_island/Pers/PersFile.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace Survival_on_island
+{
+    public static class PersFile
+    {
+        public static Pers Load(string filename)
+        {
+            var pers_file = File.ReadAllText(filename);
+            Pers pers = JsonConvert.DeserializeObject<Pers>(pers_file);
+            return pers;
+        }
+
+        public static void Save(Pers pers, string filename)
+        {
+            string json = JsonConvert.SerializeObject(pers, Formatting.Indented);
+            File.WriteAllText(filename, json);
+        }
+    }
+}
