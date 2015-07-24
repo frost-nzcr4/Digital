c309ea979581d86b04c7d0e4ef56026bba2d8375
Alexander Pervakov <frost.nzcr4@jagmort.com>
2015-07-24 10:25:18 +0300

    [Pers] Add constructor

--- Survival_on_island/Pers.cs
+++ Survival_on_island/Pers.cs
@@ -4,8 +4,35 @@ namespace Survival_on_island
 {
     public class Pers
     {
-        public Pers ()
+        public string name;
+
+        //навыки
+        public int NavSob;
+        public int NavHunt;
+        public int NavFish;
+        public int NavBuild;
+        public int NavNauka;
+        public int NavMed;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Survival_on_island.Pers"/> class.
+        /// </summary>
+        /// <param name="name1">Имя персонажа.</param>
+        /// <param name="NavSob1">Навык собирательства.</param>
+        /// <param name="NavHunt1">Навык охоты.</param>
+        /// <param name="NavFish1">Навык рыбалки.</param>
+        /// <param name="NavBuild1">Навык строительства.</param>
+        /// <param name="NavNauka1">Навык науки.</param>
+        /// <param name="NavMed1">Навык медицины.</param>
+        public Pers (string name1, int NavSob1, int NavHunt1, int NavFish1, int NavBuild1, int NavNauka1, int NavMed1)
         {
+            name = name1;
+            NavSob = NavSob1;
+            NavHunt = NavHunt1;
+            NavFish = NavFish1;
+            NavBuild = NavBuild1;
+            NavNauka = NavNauka1;
+            NavMed = NavMed1;
         }
     }
 }
