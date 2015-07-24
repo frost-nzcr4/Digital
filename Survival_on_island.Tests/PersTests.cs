62764725c85a129618013a6905af96e1eeb7069c
Alexander Pervakov <frost.nzcr4@jagmort.com>
2015-07-24 04:58:29 +0300

    Add tests for Pers class

---
+++ Survival_on_island.Tests/PersTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+using Xunit.Extensions;
+
+namespace Survival_on_island.Tests
+{
+    public class PersTests
+    {
+        [Theory]
+        [InlineData("Герой", 1, 2, 3, 4, 5, 6)]
+        public void Pers(string name, int NavSob, int NavHunt, int NavFish, int NavBuild, int NavNauka, int NavMed)
+        {
+            Items pers = new Pers(name, NavSob, NavHunt, NavFish, NavBuild, NavNauka, NavMed);
+            Assert.Equal(name, pers.name);
+            Assert.Equal(NavSob, pers.NavSob);
+            Assert.Equal(NavHunt, pers.NavHunt);
+            Assert.Equal(NavFish, pers.NavFish);
+            Assert.Equal(NavBuild, pers.NavBuild);
+            Assert.Equal(NavNauka, pers.NavNauka);
+            Assert.Equal(NavMed, pers.NavMed);
+        }
+    }
+}
